2ac2c1d2ce034a0a66944fc9b915d7dd1d1f5aa6
MifatRahman <mifat.swe@gmail.com>
2021-06-26 12:57:13 +0600

    Initial commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,73 @@
+﻿using PurchaseOrder.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PurchaseOrder.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Purchasedetail> itmlist = db.Purchasedetails.ToList<Purchasedetail>();
+                return Json(new { data = itmlist }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult ItemInsert(int id = 0)
+        {
+            if (id == 0)
+                return View(new Purchasedetail());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Purchasedetails.Where(x => x.ItemID == id).FirstOrDefault<Purchasedetail>());
+                }
+            }
+
+        }
+        [HttpPost]
+        public ActionResult ItemInsert(Purchasedetail itm)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (itm.ItemID == 0)
+                {
+                    db.Purchasedetails.Add(itm);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Save Sucessfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(itm).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Update Sucessfully" }, JsonRequestBehavior.AllowGet);
+                }
+
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Purchasedetail itm = db.Purchasedetails.Where(x => x.ItemID == id).FirstOrDefault<Purchasedetail>();
+                db.Purchasedetails.Remove(itm);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Delete Sucessfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Controllers/OrderController.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PurchaseOrder.Models;
+
+namespace PurchaseOrder.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Purchaseorder> odrlist = db.Purchaseorders.ToList<Purchaseorder>();
+                return Json(new { data = odrlist }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Purchaseorder());
+            else 
+            {
+                using (DBModel db = new DBModel()) 
+                {
+                    return View(db.Purchaseorders.Where(x=>x.RefID==id).FirstOrDefault<Purchaseorder>());
+                }
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Purchaseorder emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (emp.RefID == 0)
+                {
+                    db.Purchaseorders.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Save Sucessfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else 
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Update Sucessfully" }, JsonRequestBehavior.AllowGet);
+                }
+                
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id) 
+        {
+            using (DBModel db = new DBModel())
+            {
+                Purchaseorder emp = db.Purchaseorders.Where(x => x.RefID == id).FirstOrDefault<Purchaseorder>();
+                db.Purchaseorders.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Delete Sucessfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult NewItem() 
+        {
+            return View();
+        }
+    }
+}
